a1f0e2cdfe7d55ed45bd3752b6dd8ebe846f2ec2
Parth Behani <parth.behani@gmail.com>
2025-06-08 03:38:14 -0700

    added more ports to weather api

--- shipping-risk-api/services/port_service.py
+++ shipping-risk-api/services/port_service.py
@@ -23,13 +23,23 @@ class PortService:
             "port of oakland": {"lat": 37.7955, "lon": -122.3120, "country": "United States", "code": "USOAK"},
             "port of seattle": {"lat": 47.6062, "lon": -122.3321, "country": "United States", "code": "USSEA"},
             "port of tacoma": {"lat": 47.2529, "lon": -122.4443, "country": "United States", "code": "USTAC"},
+            "port of san francisco": {"lat": 37.7749, "lon": -122.4194, "country": "United States", "code": "USSFO"},
             
-            # North America - US East Coast
+            # North America - US East Coast & Gulf
             "port of new york": {"lat": 40.6892, "lon": -74.0445, "country": "United States", "code": "USNYC"},
             "port of savannah": {"lat": 32.0835, "lon": -81.0998, "country": "United States", "code": "USSAV"},
             "port of charleston": {"lat": 32.7767, "lon": -79.9311, "country": "United States", "code": "USCHS"},
             "port of miami": {"lat": 25.7617, "lon": -80.1918, "country": "United States", "code": "USMIA"},
             "port of houston": {"lat": 29.7604, "lon": -95.3698, "country": "United States", "code": "USHOU"},
+            "port of new orleans": {"lat": 29.9511, "lon": -90.0715, "country": "United States", "code": "USMSY"},
+            "port of baltimore": {"lat": 39.2904, "lon": -76.6122, "country": "United States", "code": "USBAL"},
+            "port of norfolk": {"lat": 36.8468, "lon": -76.2951, "country": "United States", "code": "USNFK"},
+            
+            # North America - Canada
+            "port of vancouver": {"lat": 49.2827, "lon": -123.1207, "country": "Canada", "code": "CAVAN"},
+            "port of montreal": {"lat": 45.5017, "lon": -73.5673, "country": "Canada", "code": "CAMTR"},
+            "port of halifax": {"lat": 44.6488, "lon": -63.5752, "country": "Canada", "code": "CAHAL"},
+            "port of prince rupert": {"lat": 54.3150, "lon": -130.3208, "country": "Canada", "code": "CAPRP"},
             
             # Asia - China
             "port of shanghai": {"lat": 31.2304, "lon": 121.4737, "country": "China", "code": "CNSHA"},
@@ -39,41 +49,117 @@ class PortService:
             "port of tianjin": {"lat": 39.1042, "lon": 117.2009, "country": "China", "code": "CNTSN"},
             "port of guangzhou": {"lat": 23.1291, "lon": 113.2644, "country": "China", "code": "CNGZH"},
             "port of dalian": {"lat": 38.9140, "lon": 121.6147, "country": "China", "code": "CNDLC"},
+            "port of xiamen": {"lat": 24.4798, "lon": 118.0819, "country": "China", "code": "CNXMN"},
+            "port of suzhou": {"lat": 31.2983, "lon": 120.5831, "country": "China", "code": "CNSUZ"},
             
-            # Asia - Other
+            # Asia - India
+            "port of mumbai": {"lat": 19.0760, "lon": 72.8777, "country": "India", "code": "INMAA"},
+            "port of chennai": {"lat": 13.0827, "lon": 80.2707, "country": "India", "code": "INMAA"},
+            "port of kolkata": {"lat": 22.5726, "lon": 88.3639, "country": "India", "code": "INCCU"},
+            "port of kochi": {"lat": 9.9312, "lon": 76.2673, "country": "India", "code": "INCOK"},
+            "port of visakhapatnam": {"lat": 17.6868, "lon": 83.2185, "country": "India", "code": "INVTZ"},
+            "port of jawaharlal nehru": {"lat": 18.9480, "lon": 72.9881, "country": "India", "code": "INNSA"},
+            
+            # Asia - Southeast Asia
             "port of singapore": {"lat": 1.2966, "lon": 103.8517, "country": "Singapore", "code": "SGSIN"},
+            "port of bangkok": {"lat": 13.7563, "lon": 100.5018, "country": "Thailand", "code": "THBKK"},
+            "port of ho chi minh city": {"lat": 10.8231, "lon": 106.6297, "country": "Vietnam", "code": "VNSGN"},
+            "port of jakarta": {"lat": -6.1087, "lon": 106.3694, "country": "Indonesia", "code": "IDJKT"},
+            "port of manila": {"lat": 14.5995, "lon": 120.9842, "country": "Philippines", "code": "PHMNL"},
+            "port of haiphong": {"lat": 20.8449, "lon": 106.6881, "country": "Vietnam", "code": "VNHPH"},
+            "port of kuala lumpur": {"lat": 3.1390, "lon": 101.6869, "country": "Malaysia", "code": "MYKUL"},
+            
+            # Asia - Other East Asia
             "port of busan": {"lat": 35.1796, "lon": 129.0756, "country": "South Korea", "code": "KRPUS"},
             "port of hong kong": {"lat": 22.3193, "lon": 114.1694, "country": "Hong Kong", "code": "HKHKG"},
             "port of kobe": {"lat": 34.6901, "lon": 135.1956, "country": "Japan", "code": "JPUKB"},
             "port of yokohama": {"lat": 35.4437, "lon": 139.6380, "country": "Japan", "code": "JPYOK"},
             "port of tokyo": {"lat": 35.6762, "lon": 139.6503, "country": "Japan", "code": "JPTYO"},
             "port of kaohsiung": {"lat": 22.6273, "lon": 120.3014, "country": "Taiwan", "code": "TWKHH"},
+            "port of nagoya": {"lat": 35.1815, "lon": 136.9066, "country": "Japan", "code": "JPNGO"},
+            "port of incheon": {"lat": 37.4563, "lon": 126.7052, "country": "South Korea", "code": "KRICN"},
             
-            # Europe
+            # Europe - Northern Europe
             "port of rotterdam": {"lat": 51.9225, "lon": 4.4792, "country": "Netherlands", "code": "NLRTM"},
             "port of antwerp": {"lat": 51.2993, "lon": 4.4014, "country": "Belgium", "code": "BEANR"},
             "port of hamburg": {"lat": 53.5511, "lon": 9.9937, "country": "Germany", "code": "DEHAM"},
             "port of felixstowe": {"lat": 51.9640, "lon": 1.3518, "country": "United Kingdom", "code": "GBFXT"},
             "port of le havre": {"lat": 49.4944, "lon": 0.1079, "country": "France", "code": "FRLEH"},
+            "port of bremen": {"lat": 53.0793, "lon": 8.8017, "country": "Germany", "code": "DEBRE"},
+            "port of london": {"lat": 51.5074, "lon": -0.1278, "country": "United Kingdom", "code": "GBLON"},
+            "port of gothenburg": {"lat": 57.7089, "lon": 11.9746, "country": "Sweden", "code": "SEGOT"},
+            "port of oslo": {"lat": 59.9139, "lon": 10.7522, "country": "Norway", "code": "NOOSL"},
+            
+            # Europe - Southern Europe
             "port of genoa": {"lat": 44.4056, "lon": 8.9463, "country": "Italy", "code": "ITGOA"},
             "port of barcelona": {"lat": 41.3851, "lon": 2.1734, "country": "Spain", "code": "ESBCN"},
             "port of valencia": {"lat": 39.4699, "lon": -0.3763, "country": "Spain", "code": "ESVLC"},
+            "port of piraeus": {"lat": 37.9755, "lon": 23.7348, "country": "Greece", "code": "GRPIR"},
+            "port of marseille": {"lat": 43.2965, "lon": 5.3698, "country": "France", "code": "FRMRS"},
+            "port of naples": {"lat": 40.8518, "lon": 14.2681, "country": "Italy", "code": "ITNAP"},
+            "port of algeciras": {"lat": 36.1408, "lon": -5.4526, "country": "Spain", "code": "ESALG"},
+            "port of gioia tauro": {"lat": 38.4244, "lon": 15.8897, "country": "Italy", "code": "ITGIT"},
             
             # Middle East
             "port of dubai": {"lat": 25.2048, "lon": 55.2708, "country": "UAE", "code": "AEDXB"},
             "port of jebel ali": {"lat": 25.0118, "lon": 55.0618, "country": "UAE", "code": "AEJEA"},
+            "port of abu dhabi": {"lat": 24.4539, "lon": 54.3773, "country": "UAE", "code": "AEAUH"},
+            "port of jeddah": {"lat": 21.4858, "lon": 39.1925, "country": "Saudi Arabia", "code": "SAJED"},
+            "port of dammam": {"lat": 26.4207, "lon": 50.0888, "country": "Saudi Arabia", "code": "SADMM"},
+            "port of bandar abbas": {"lat": 27.1865, "lon": 56.2808, "country": "Iran", "code": "IRBND"},
+            "port of sohar": {"lat": 24.3415, "lon": 56.7539, "country": "Oman", "code": "OMSOH"},
+            "port of kuwait": {"lat": 29.3759, "lon": 47.9774, "country": "Kuwait", "code": "KWKWI"},
+            
+            # Africa - North Africa
+            "port of alexandria": {"lat": 31.2001, "lon": 29.9187, "country": "Egypt", "code": "EGALX"},
+            "port of suez": {"lat": 29.9668, "lon": 32.5498, "country": "Egypt", "code": "EGSUZ"},
+            "port of casablanca": {"lat": 33.5731, "lon": -7.5898, "country": "Morocco", "code": "MACAS"},
+            "port of tunis": {"lat": 36.8065, "lon": 10.1815, "country": "Tunisia", "code": "TNTUN"},
+            
+            # Africa - West Africa
+            "port of lagos": {"lat": 6.5244, "lon": 3.3792, "country": "Nigeria", "code": "NGLOS"},
+            "port of tema": {"lat": 5.6037, "lon": -0.0163, "country": "Ghana", "code": "GHTEM"},
+            "port of abidjan": {"lat": 5.3600, "lon": -4.0083, "country": "Ivory Coast", "code": "CIABJ"},
+            "port of dakar": {"lat": 14.6937, "lon": -17.4441, "country": "Senegal", "code": "SNDKR"},
+            
+            # Africa - East & South Africa
+            "port of durban": {"lat": -29.8587, "lon": 31.0218, "country": "South Africa", "code": "ZADUR"},
+            "port of cape town": {"lat": -33.9249, "lon": 18.4241, "country": "South Africa", "code": "ZACPT"},
+            "port of dar es salaam": {"lat": -6.7924, "lon": 39.2083, "country": "Tanzania", "code": "TZDAR"},
+            "port of mombasa": {"lat": -4.0435, "lon": 39.6682, "country": "Kenya", "code": "KEMBA"},
+            "port of port elizabeth": {"lat": -33.9580, "lon": 25.6022, "country": "South Africa", "code": "ZAPLZ"},
             
             # South America
             "port of santos": {"lat": -23.9618, "lon": -46.3322, "country": "Brazil", "code": "BRSSZ"},
             "port of buenos aires": {"lat": -34.6118, "lon": -58.3960, "country": "Argentina", "code": "ARBUE"},
+            "port of callao": {"lat": -12.0464, "lon": -77.1428, "country": "Peru", "code": "PECLL"},
+            "port of cartagena": {"lat": 10.3910, "lon": -75.4794, "country": "Colombia", "code": "COCTG"},
+            "port of rio de janeiro": {"lat": -22.9068, "lon": -43.1729, "country": "Brazil", "code": "BRRIO"},
+            "port of valparaiso": {"lat": -33.0472, "lon": -71.6127, "country": "Chile", "code": "CLVAP"},
+            "port of montevideo": {"lat": -34.9011, "lon": -56.1645, "country": "Uruguay", "code": "UYMVD"},
+            "port of fortaleza": {"lat": -3.7172, "lon": -38.5433, "country": "Brazil", "code": "BRFOR"},
             
-            # Africa
-            "port of durban": {"lat": -29.8587, "lon": 31.0218, "country": "South Africa", "code": "ZADUR"},
-            "port of cape town": {"lat": -33.9249, "lon": 18.4241, "country": "South Africa", "code": "ZACPT"},
-            
-            # Australia
+            # Australia & Oceania
             "port of sydney": {"lat": -33.8688, "lon": 151.2093, "country": "Australia", "code": "AUSYD"},
             "port of melbourne": {"lat": -37.8136, "lon": 144.9631, "country": "Australia", "code": "AUMEL"},
+            "port of brisbane": {"lat": -27.4698, "lon": 153.0251, "country": "Australia", "code": "AUBNE"},
+            "port of fremantle": {"lat": -32.0569, "lon": 115.7445, "country": "Australia", "code": "AUFRE"},
+            "port of adelaide": {"lat": -34.9285, "lon": 138.6007, "country": "Australia", "code": "AUADL"},
+            "port of auckland": {"lat": -36.8485, "lon": 174.7633, "country": "New Zealand", "code": "NZAKL"},
+            "port of tauranga": {"lat": -37.6870, "lon": 176.1651, "country": "New Zealand", "code": "NZTAU"},
+            
+            # Caribbean & Central America
+            "port of kingston": {"lat": 17.9712, "lon": -76.7928, "country": "Jamaica", "code": "JMKIN"},
+            "port of colon": {"lat": 9.3547, "lon": -79.9009, "country": "Panama", "code": "PACLN"},
+            "port of san juan": {"lat": 18.4655, "lon": -66.1057, "country": "Puerto Rico", "code": "PRSJU"},
+            "port of freeport": {"lat": 26.5285, "lon": -78.6957, "country": "Bahamas", "code": "BSFPO"},
+            
+            # Russia & Eastern Europe
+            "port of st petersburg": {"lat": 59.9311, "lon": 30.3609, "country": "Russia", "code": "RULED"},
+            "port of novorossiysk": {"lat": 44.7230, "lon": 37.7687, "country": "Russia", "code": "RUNVS"},
+            "port of vladivostok": {"lat": 43.1056, "lon": 131.8735, "country": "Russia", "code": "RUVVO"},
+            "port of gdansk": {"lat": 54.3520, "lon": 18.6466, "country": "Poland", "code": "PLGDN"},
+            "port of constanta": {"lat": 44.1598, "lon": 28.6348, "country": "Romania", "code": "ROCND"},
         }
         
         # Create reverse lookup by variations
